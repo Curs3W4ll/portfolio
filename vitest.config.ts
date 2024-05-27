9d1cf7ba34ce6ca652d2170f7a90fae21fb0351f
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-05-27 10:45:15 -0400

    #4 :wrench: Add JUnit unit tests reports to GitLab MRs

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { configDefaults } from "vitest/config";
 
 export default defineVitestConfig({
   test: {
+    reporters: ["default", "junit"],
+    outputFile: {
+      junit: "junit-report.xml",
+    },
     coverage: {
       exclude: [...configDefaults.exclude, "nuxt.config.ts", "**/*.vue", ".nuxt/*"],
       reporter: ["cobertura", "text", "html"],

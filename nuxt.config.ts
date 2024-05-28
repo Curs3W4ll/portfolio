30318c5b51b4d597372860a14d17822db5228c80
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-05-28 10:41:04 -0400

    #5 :wrench: Add E2E tests to the project CI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
   css: ["@/assets/main.css"],
   modules: ["@nuxt/eslint", "@nuxt/test-utils/module"],
   eslint: {

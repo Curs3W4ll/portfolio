18aa9f88b4bb729aafcccbace4bb05af302d987b
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-11-28 21:47:33 +0100

    #22 #26 :sparkles: Add SEO references

---
+++ utils/config.ts
@@ -0,0 +1,12 @@
+export default {
+  seo: {
+    name: "Corentin Hervaud",
+    description:
+      "Corentin Hervaud is a software developer specializing in creating innovative and modern websites and software using the latest technologies.",
+    image: "https://hervaud.fr/og.png",
+    locale: "en_US",
+    type: "website",
+    url: "http://hervaud.fr",
+    color: "#C980E6",
+  },
+};

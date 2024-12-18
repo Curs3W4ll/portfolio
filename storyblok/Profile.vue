0d32bdbdc917fb993c30e358001b740b87b4606b
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-12-18 12:31:12 +0100

    #13 :sparkles: [Profile] Add link to main name

--- storyblok/Profile.vue
+++ storyblok/Profile.vue
@@ -8,7 +8,9 @@ defineProps<{
 
 <template>
   <div v-editable="blok" class="space-y-3">
-    <h1 class="text-4xl sm:text-5xl font-extrabold shrink-0 whitespace-nowrap">{{ blok.name }}</h1>
+    <NuxtLink to="/">
+      <h1 class="text-4xl sm:text-5xl font-extrabold shrink-0 whitespace-nowrap">{{ blok.name }}</h1>
+    </NuxtLink>
     <h2 class="text-xl sm:text-2xl font-bold">{{ blok.job }}</h2>
     <h3>{{ blok.description }}</h3>
   </div>

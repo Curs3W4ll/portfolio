5183bcd4e3efe318d73bfdbaa0883a2f92aa2b94
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-12-18 12:31:26 +0100

    #13 :sparkles: Add app.vue file to add loading indicator and others

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtRouteAnnouncer />
+  <NuxtLoadingIndicator color="repeating-linear-gradient(to right,#C980E6 0%,#5FDFC8 100%)" />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>

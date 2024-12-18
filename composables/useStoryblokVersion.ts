8e65f64ebf0066a76834e515b49291dc25e7dc5d
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-12-18 14:28:17 +0100

    #13 :truck: Remove errors ignore as Sentry types now works as expected

--- composables/useStoryblokVersion.ts
+++ composables/useStoryblokVersion.ts
@@ -5,7 +5,6 @@ export const useStoryblokVersion = () => {
 
   const version = config.public.storyblokVersion;
   if (version != "draft" && version != "published") {
-    // @ts-expect-error 2339 BUG: For some reasons Sentry fails to build types file
     Sentry.captureMessage(`Given Storyblok version (${config.public.storyblokVersion}) is invalid`);
   }
 

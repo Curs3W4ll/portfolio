0700a6403630b3b138073cef688e4417719a0c92
Curs3W4ll <corentin.hervaud@epitech.eu>
2024-12-19 18:40:10 +0100

    #44 :sparkles: [Profile] Add resume button

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useFlag } from "@unleash/proxy-client-vue";
+
 const sbVersion = useStoryblokVersion();
 
 const profileStory = await useAsyncStoryblok("profile", {
@@ -7,15 +9,34 @@ const profileStory = await useAsyncStoryblok("profile", {
 const aboutStory = await useAsyncStoryblok("about", {
   version: sbVersion,
 });
+
+const displayResumeButton = useFlag("display-resume");
 </script>
 
 <template>
-  <div class="gap-24 px-10 *:pt-28 max-w-screen-xl lg:flex lg:justify-between lg:items-start min-h-screen xl:container">
+  <div
+    class="gap-24 px-10 *:pt-28 max-w-screen-xl lg:flex lg:justify-between lg:items-start min-h-screen xl:container"
+    data-test="root"
+  >
     <StoryblokComponent
       v-if="profileStory"
       class="lg:sticky lg:top-0 lg:pb-28 lg:h-screen"
       :blok="profileStory.content"
+      data-test="profile-section"
     />
-    <StoryblokComponent v-if="aboutStory" class="pb-28" :blok="aboutStory.content" />
+    <div class="pb-28 flex flex-col gap-32">
+      <StoryblokComponent v-if="aboutStory" :blok="aboutStory.content" />
+      <Button
+        v-if="displayResumeButton"
+        class="mx-auto"
+        label="See my resume"
+        size="large"
+        icon="pi pi-file-pdf"
+        as="a"
+        href="/resume.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      />
+    </div>
   </div>
 </template>

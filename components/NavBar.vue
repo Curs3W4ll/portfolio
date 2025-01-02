3463b7b7c850df707092f34b8e6a5e478df6d6a1
Curs3W4ll <corentin.hervaud@epitech.eu>
2025-01-02 16:16:42 +0100

    #29 :bug: [NavBar] Select section as active when it's displayed from top to bottom on the screen

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,18 +3,32 @@ const anchors = ["about", "experience", "projects"];
 const activeAnchor = ref(anchors[0]);
 
 const handleScroll = () => {
+  let selectedAnchor: string | undefined = undefined;
+
   for (const anchor of anchors) {
     const rect = document.getElementById(anchor)?.getBoundingClientRect();
 
-    if (rect && rect.bottom >= 0) {
-      activeAnchor.value = anchor;
+    if (!rect) {
+      continue;
+    }
+    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+      selectedAnchor = anchor;
       break;
     }
+    if (rect.bottom >= 0 && !selectedAnchor) {
+      selectedAnchor = anchor;
+    }
   }
+
+  activeAnchor.value = selectedAnchor ?? anchors[0];
 };
 
 onMounted(() => {
   window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
 });
 </script>
 

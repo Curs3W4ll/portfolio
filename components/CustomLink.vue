7c9fce7f014987006f66972552e4b2afaf323ff9
GitLab CI <ci.epitech@gmail.com>
2025-01-15 10:37:39 +0000

    :art: Format code

--- components/CustomLink.vue
+++ components/CustomLink.vue
@@ -6,8 +6,8 @@ defineProps<NuxtLinkProps>();
 
 <template>
   <NuxtLink
-    class="font-bold hover:text-primary after:content-['↗\00a0'] after:inline-block after:hover:motion-safe:scale-110 after:hover:motion-safe:-translate-y-1 after:hover:motion-safe:translate-x-0.5 after:motion-safe:transition-transform after:ease-in-out after:duration-300"
     v-bind="$props"
+    class="font-bold hover:text-primary after:content-['↗\00a0'] after:inline-block after:hover:motion-safe:scale-110 after:hover:motion-safe:-translate-y-1 after:hover:motion-safe:translate-x-0.5 after:motion-safe:transition-transform after:ease-in-out after:duration-300"
   >
     <slot />
   </NuxtLink>

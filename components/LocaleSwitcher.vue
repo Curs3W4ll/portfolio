49453a1a067db529c24965b279cc002e1770f797
Curs3W4ll <corentin.hervaud@epitech.eu>
2025-01-17 21:39:07 +0100

    #16 :sparkles: Add locale switcher

---
+++ components/LocaleSwitcher.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { locale, locales } = useI18n();
+</script>
+
+<template>
+  <div>
+    <p class="font-bold"><i class="pi pi-language" /> Language</p>
+    <div class="space-x-2 pl-3">
+      <SwitchLocalePathLink
+        v-for="l in locales"
+        :key="l.code"
+        class="uppercase"
+        :class="[locale === l.code ? 'text-white underline' : 'text-white-dark']"
+        :locale="l.code"
+      >
+        {{ l.code }}
+      </SwitchLocalePathLink>
+    </div>
+  </div>
+</template>

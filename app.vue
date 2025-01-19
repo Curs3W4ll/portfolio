07594336337d7f15f13eb1abf9d6c3bd03e948e5
Curs3W4ll <corentin.hervaud@epitech.eu>
2025-01-19 18:29:50 +0100

    #16 :sparkles: [AnimatedEyes] Add feature flags to toggle animated eyes popup visibility

--- app.vue
+++ app.vue
@@ -1,7 +1,19 @@
+<script setup lang="ts">
+import { useFlag } from "@unleash/proxy-client-vue";
+
+const displayEyesPopup = ref<boolean>(false);
+
+onMounted(() => {
+  const displayAnimatedEyesFlag = useFlag("display-animated-eyes");
+
+  syncRef(displayEyesPopup, displayAnimatedEyesFlag, { direction: "rtl" });
+});
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLoadingIndicator color="repeating-linear-gradient(to right,#C980E6 0%,#5FDFC8 100%)" />
-  <EyesPopup />
+  <EyesPopup v-if="displayEyesPopup" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>

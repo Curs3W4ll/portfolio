e394d666f21f600cd3b129c366bfb04a0589049a
Curs3W4ll <corentin.hervaud@epitech.eu>
2025-01-20 11:02:32 +0100

    #59 :bug: [Eyes popup] Upgrade z-index of eyes popup so it's displayed in front of nav links

--- components/EyesPopup.vue
+++ components/EyesPopup.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed top-1/2 left-0 -translate-y-1/2 -translate-x-40 flex items-center hover:translate-x-0 motion-safe:transition-transform ease-in-out duration-200"
+    class="fixed top-1/2 left-0 -translate-y-1/2 -translate-x-40 flex items-center hover:translate-x-0 motion-safe:transition-transform ease-in-out duration-200 z-10"
   >
     <div class="rounded-r border-8 border-l-0 border-black w-40 h-52">
       <div class="bg-gradient-to-br from-primary-600 to-primary-800 rounded-r w-full h-full p-3">

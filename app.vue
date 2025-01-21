8179196b67fdb070934d5eff4eee130762bfb627
Curs3W4ll <corentin.hervaud@epitech.eu>
2025-01-21 16:28:20 +0100

    #15 :sparkles: [Language switcher] Conditionnally display language switcher with feature flag

--- app.vue
+++ app.vue
@@ -2,18 +2,21 @@
 import { useFlag } from "@unleash/proxy-client-vue";
 
 const displayEyesPopup = ref<boolean>(false);
+const displayLanguageSwitcher = ref<boolean>(false);
 
 onMounted(() => {
   const displayAnimatedEyesFlag = useFlag("display-animated-eyes");
+  const displayLanguageSwitcherFlag = useFlag("display-language-switcher");
 
   syncRef(displayEyesPopup, displayAnimatedEyesFlag, { direction: "rtl" });
+  syncRef(displayLanguageSwitcher, displayLanguageSwitcherFlag, { direction: "rtl" });
 });
 </script>
 
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLoadingIndicator color="repeating-linear-gradient(to right,#C980E6 0%,#5FDFC8 100%)" />
-  <EyesPopup v-if="displayEyesPopup" />
+  <EyesPopup v-if="displayEyesPopup" :display-language-switcher="displayLanguageSwitcher" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>

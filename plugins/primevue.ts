6d01e4a27713ac201280716d360060c2962ca090
Curs3W4ll <corentin.hervaud@epitech.eu>
2025-04-19 11:50:03 +0200

    #79 :bug: Import primevue tooltip directive

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,7 +1,10 @@
 import PrimeVue from "primevue/config";
+import Tooltip from "primevue/tooltip";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, {
     unstyled: true,
   });
+
+  nuxtApp.vueApp.directive("tooltip", Tooltip);
 });

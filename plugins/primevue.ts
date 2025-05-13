ddeb83c6d5e4ba34b26b72e82a63b3786a3c624c
Curs3W4ll <corentin.hervaud@epitech.eu>
2025-05-13 22:03:12 +0200

    #79 :fire: [primevue] Remove primevue tooltip component

--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -1,10 +1,7 @@
 import PrimeVue from "primevue/config";
-import Tooltip from "primevue/tooltip";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, {
     unstyled: true,
   });
-
-  nuxtApp.vueApp.directive("tooltip", Tooltip);
 });

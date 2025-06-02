3cfb79c007016d6d324c2e68f2c9cd44ea9f3ed1
Curs3W4ll <corentin.hervaud@epitech.eu>
2025-06-02 08:07:21 +0200

    #90 :bug: [Eyes popup] Fix popup not being able to be closed when clicking on it

--- components/EyesPopup.vue
+++ components/EyesPopup.vue
@@ -4,8 +4,10 @@ defineProps<{
   displayThemeSwitcher: boolean;
 }>();
 
-const show = useState(() => false);
-const toggle = useToggle(show);
+const show = useState("show-popup", () => false);
+const toggle = () => {
+  show.value = !show.value;
+};
 </script>
 
 <template>
@@ -13,7 +15,7 @@ const toggle = useToggle(show);
     class="fixed top-1/2 left-0 -translate-y-1/2 -translate-x-40 flex items-center hover:translate-x-0 motion-safe:transition-transform ease-in-out duration-200 z-10"
     :class="{ 'translate-x-0': show }"
   >
-    <div class="rounded-r border-8 border-l-0 border-black w-40 h-52">
+    <div class="rounded-r border-8 border-l-0 border-black w-40 h-52" data-test="popup">
       <div
         class="bg-linear-to-br from-primary-200 to-primary-400 dark:from-primary-700 dark:to-primary-800 rounded-r w-full h-full p-3 flex flex-col justify-around"
       >

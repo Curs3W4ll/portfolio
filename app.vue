cff7aeda21ce3957c7e7ac1018106035d1f68654
Curs3W4ll <corentin.hervaud@epitech.eu>
2025-08-12 00:03:54 +0200

    #96 #96 :art: Break code format to test push with CI job token

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtRouteAnnouncer />
-  <NuxtLoadingIndicator color="repeating-linear-gradient(to right,#C980E6 0%,#5FDFC8 100%)" />
+			  <NuxtLoadingIndicator color="repeating-linear-gradient(to right,#C980E6 0%,#5FDFC8 100%)" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
